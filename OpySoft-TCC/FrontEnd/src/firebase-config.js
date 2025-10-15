bc0f74054fae7dc377fb7c6539c8b5846464881e
TioXin <arthurgoes200@gmail.com>
2025-10-15 20:43:30 -0300

    Removendo Key Do Firebase Do Projeto

--- OpySoft-TCC/FrontEnd/src/firebase-config.js
+++ OpySoft-TCC/FrontEnd/src/firebase-config.js
@@ -5,13 +5,7 @@ import { getAuth } from 'firebase/auth';
 
 // Substitua PELAS SUAS PRÓPRIAS CHAVES DO PROJETO FIREBASE!
 const firebaseConfig = {
-  apiKey: "AIzaSyD1ZxgQwzLBcHsYSTN93psg11PslckYsnQ",
-  authDomain: "opysoft.firebaseapp.com",
-  projectId: "opysoft",
-  storageBucket: "opysoft.firebasestorage.app",
-  messagingSenderId: "173188857687",
-  appId: "1:173188857687:web:c579ea319ffa81cdf3c812",
-  measurementId: "G-9DMVCE4X5J"
+
 };
 
 const app = initializeApp(firebaseConfig);
